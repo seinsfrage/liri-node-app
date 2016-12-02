5bbdc1735d96777868d8c54a0287ded1bfd5b26c
Daniel Smith <docsmith.ut@gmail.com>
2016-12-02 02:32:41 -0600

    liri is complete

--- liri.js
+++ liri.js
@@ -1,14 +1,155 @@
-//Liri takes the following commands
+//liri takes the following commands
 // * my-tweets
 // * spotify-this-song
 // * movie-this
 // * do-what-it-says
 
-//these add other programs to this one
-var dataKeys = require("./keys.js");
+//vars for 'required' resources
+var dataKeys = require('./keys.js');
 var fs = require('fs'); //file system
 var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 
+//log.txt function
+var writeToLog = function(data) {
+  fs.appendFile("log.txt", '\r\n\r\n');
 
+  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("log.txt was updated!");
+  });
+};
+
+//find song function 1a - spotify-this-song (artist variant) command
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+
+//find song function 1b - spotify-this-song (song) command
+var getMeSpotify = function(songName) {
+  //default: Blink 182's What's my age again
+  if (songName === undefined) {
+    songName = 'Head Like A Hole';
+  };
+
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
+
+    var songs = data.tracks.items;
+    var data = []; //empty array to hold data
+
+    for (var i = 0; i < songs.length; i++) {
+      data.push({
+        'artist(s)': songs[i].artists.map(getArtistNames),
+        'song name: ': songs[i].name,
+        'preview song: ': songs[i].preview_url,
+        'album: ': songs[i].album.name,
+      });
+    }
+    console.log(data);
+    writeToLog(data);
+  });
+};
+
+//retrieve tweets function - my-tweets command
+var getTweets = function() {
+  var client = new twitter(dataKeys.twitterKeys);
+
+  var params = { screen_name: '_chaosmosis', count: 20 };
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+    if (!error) {
+      var data = []; //empty array to hold data
+      for (var i = 0; i < tweets.length; i++) {
+        data.push({
+            'created at: ' : tweets[i].created_at,
+            'Tweets: ' : tweets[i].text,
+        });
+      }
+      console.log(data);
+      writeToLog(data);
+    }
+  });
+};
+
+//find movie function - movie-this command
+var getMeMovie = function(movieName) {
+//default: Mr Nobody
+  if (movieName === undefined) {
+    movieName = 'Mr Nobody';
+  }
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&r=json";
+
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'Rated: ' : jsonData.Rated,
+      'IMDB Rating: ' : jsonData.imdbRating,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language,
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
+      'Rotten Tomatoes URL: ' : jsonData.tomatoURL,
+  });
+      console.log(data);
+      writeToLog(data);
+}
+  });
+
+}
+//do what it says function - do-what-it-says command
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    writeToLog(data);
+    var dataArr = data.split(',')
+
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+
+  });
+}
+
+//function-command "selector"
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      getTweets();
+      break;
+    case 'spotify-this-song':
+      getMeSpotify(functionData);
+      break;
+    case 'movie-this':
+      getMeMovie(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI doesn\'t know that');
+  }
+}
+// js and command line parsing
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
